f0bc8a0c81a507577923ff268a5d6f7874567fd0
JSai <jsaivarahi@gmail.com>
2019-01-06 05:51:28 +0000

    sairam

--- SFM_train.py
+++ SFM_train.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
-import numpy as np
 import cPickle as pickle
-from collections import OrderedDict
-import sys
 import os
+import sys
+from collections import OrderedDict
 
-lib_path = 'path/to/lib'
+import numpy as np
+
+lib_path = 'lib'
 
 sys.path.append(lib_path)
-import midi
-from midi.utils import midiread, midiwrite
+from midi.utils import midiread
 
 import theano
+from theano import tensor as T, function, printing
 import theano.tensor as T
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+import IPython
+import pandas as pd
+acc = 0
 
 def pre_proc(data):
 	norm = np.sqrt(np.sum(data**2, axis=1))
@@ -27,13 +31,13 @@ def pre_proc3(data):
 	return data
 
 def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x), axis=0)
+	return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 def unzip(zipped):
-    new_params = OrderedDict()
-    for kk, vv in zipped.items():
-        new_params[kk] = vv.get_value()
-    return new_params
+	new_params = OrderedDict()
+	for kk, vv in zipped.items():
+		new_params[kk] = vv.get_value()
+	return new_params
 
 class MusicDataProvider:
 	def __init__(self, dir_path, set_name, pitch_range, dt, batch_size, shuffle=True):
@@ -54,7 +58,6 @@ class MusicDataProvider:
 			f = self.dir_path+'/'+self.set_name+'/'+m
 			wave = midiread(f, self.pitch_range, self.dt)
 			self.data.append(wave.piano_roll)
-
 		self.data_list = np.array(range(self.data_num))
 		if self.shuffle:
 			np.random.shuffle(self.data_list)
@@ -111,6 +114,7 @@ def SFM(tparams, x, omega, opts):
 		Im_s = f*Im_s_+T.outer(g*i, T.sin(omega*t_))
 
 		A = T.sqrt(Re_s**2+Im_s**2)
+		#theano.printing.pydotprint(A, outfile="amplitude", var_with_name_simple=True)
 
 		def __feq(U_o, W_o, V_o, b_o, W_z, b_z, A_k, z_k):
 			o = T.nnet.sigmoid(T.dot(U_o, A_k)+T.dot(W_o, z_)+T.dot(V_o, x_)+b_o)
@@ -164,30 +168,29 @@ def Adaptive_SFM(tparams, x, omega, opts):
 
 def adadelta(tparams, train_feats, train_nframe, omega, cost, grads):
 
-    shared_grads   = [theano.shared(p.get_value() * np.asarray(0.), name='%s_grad' % k)   for k, p in tparams.items()]
-    running_grads2 = [theano.shared(p.get_value() * np.asarray(0.), name='%s_rgrad2' % k) for k, p in tparams.items()]
-    running_delta2 = [theano.shared(p.get_value() * np.asarray(0.), name='%s_rdelta2' % k)   for k, p in tparams.items()]
-    n = len(shared_grads)
+	shared_grads   = [theano.shared(p.get_value() * np.asarray(0.), name='%s_grad' % k)   for k, p in tparams.items()]
+	running_grads2 = [theano.shared(p.get_value() * np.asarray(0.), name='%s_rgrad2' % k) for k, p in tparams.items()]
+	running_delta2 = [theano.shared(p.get_value() * np.asarray(0.), name='%s_rdelta2' % k)   for k, p in tparams.items()]
+	n = len(shared_grads)
 
-    sgup = [(shared_grads[i], grads[i]) for i in range(n)]
-    rg2up = [(running_grads2[i], 0.95*running_grads2[i] + 0.05*(grads[i]**2)) for i in range(n)]
+	sgup = [(shared_grads[i], grads[i]) for i in range(n)]
+	rg2up = [(running_grads2[i], 0.95*running_grads2[i] + 0.05*(grads[i]**2)) for i in range(n)]
 
-    f_grad = theano.function([train_feats, train_nframe, omega], cost, updates=sgup + rg2up,
-                                    on_unused_input='ignore', name='adadelta_f_grad', allow_input_downcast=True)
+	f_grad = theano.function([train_feats, train_nframe, omega], cost, updates=sgup + rg2up,
+									on_unused_input='ignore', name='adadelta_f_grad', allow_input_downcast=True)
 
-    delta_param = [-T.sqrt(rd2+1e-6) / T.sqrt(rg2+1e-6) * sg  for sg, rd2, rg2 in zip(shared_grads, running_delta2, running_grads2)]
-    rd2up = [(rd2, 0.95*rd2 + 0.05*(dp**2)) for rd2, dp in zip(running_delta2, delta_param)]
-    paramup = [(p, p+dp) for p, dp in zip(tparams.values(), delta_param)]
+	delta_param = [-T.sqrt(rd2+1e-6) / T.sqrt(rg2+1e-6) * sg  for sg, rd2, rg2 in zip(shared_grads, running_delta2, running_grads2)]
+	rd2up = [(rd2, 0.95*rd2 + 0.05*(dp**2)) for rd2, dp in zip(running_delta2, delta_param)]
+	paramup = [(p, p+dp) for p, dp in zip(tparams.values(), delta_param)]
 
-    lr = T.scalar('lr')
-    f_update = theano.function([lr], [], updates=rd2up + paramup, on_unused_input='ignore', name='adadelta_f_update', allow_input_downcast=True)
+	lr = T.scalar('lr')
+	f_update = theano.function([lr], [], updates=rd2up + paramup, on_unused_input='ignore', name='adadelta_f_update', allow_input_downcast=True)
 
-    return f_grad, f_update
+	return f_grad, f_update
 
 def eval_batch(batch_fea, nframe, tparams, omega, options):
 	x = T.fmatrix('x')
-	tp = theano.function([x], SFM(tparams, x, omega, options), name='feat', allow_input_downcast=True)
-
+	tp = theano.function([x], Adaptive_SFM(tparams, x, omega, options), name='feat', allow_input_downcast=True)
 	n = nframe.shape[0]
 	log_like=[None]*n
 	for i in range(n):
@@ -196,42 +199,49 @@ def eval_batch(batch_fea, nframe, tparams, omega, options):
 		x_input = batch_fea[x_start:x_end]
 
 		outs = tp(x_input)
+		df = pd.DataFrame(outs)
+		name = "excel" + str(acc) + str(i) + ".xlsx"
+		print "this is name"
+		print name
+		#df.to_excel(name, index=False)
 		log_like_fr = [None]*(nframe[i]-1)
 		for j in range(nframe[i]-1):
 			log_like_fr[j] = np.dot(x_input[j+1], np.log(softmax(outs[j])))
 		log_like[i] = np.mean(log_like_fr)
+	global acc
+	acc += 1
 	return log_like
 
 def build_model_train(tparams, train_opt):
-    t_f = T.fmatrix('train_feats')
-    t_n = T.ivector('train_nframe')
-    omega = T.fvector('omega')
+	t_f = T.fmatrix('train_feats')
+	t_n = T.ivector('train_nframe')
+	omega = T.fvector('omega')
 
-    """ Cost """
-    trng = RandomStreams()
-    def _cost(i, cost_prev):
-        x_start = T.sum(t_n[:i])
-        x_end = x_start+t_n[i]
-        x = t_f[x_start:x_end]
+	""" Cost """
+	trng = RandomStreams()
+	def _cost(i, cost_prev):
+		x_start = T.sum(t_n[:i])
+		x_end = x_start+t_n[i]
+		x = t_f[x_start:x_end]
 
-        outs = SFM(tparams, x, omega, train_opt)
-        log_lik = T.nlinalg.trace(T.dot(x[1:], T.log(T.nnet.softmax(outs[:-1])).transpose()))
-        
-        total_cost = cost_prev+(-log_lik)
-        return total_cost
+		outs = Adaptive_SFM(tparams, x, omega, train_opt)
+		log_lik = T.nlinalg.trace(T.dot(x[1:], T.log(T.nnet.softmax(outs[:-1])).transpose()))
 
-    t_l2 = T.arange(t_n.shape[0], dtype='int64')
-    costs, updates = theano.scan(_cost, sequences=[t_l2], outputs_info=T.alloc(np.float64(0.)), name='log_likelyhood', n_steps=t_n.shape[0])
-    cost = costs[-1]
-    
+		total_cost = cost_prev+(-log_lik)
+		return total_cost
 
-    """ Gradient """
-    grads = theano.grad(cost, wrt=list(tparams.values()))
-    
+	t_l2 = T.arange(t_n.shape[0], dtype='int64')
+	costs, updates = theano.scan(_cost, sequences=[t_l2], outputs_info=T.alloc(np.float64(0.)), name='log_likelyhood', n_steps=t_n.shape[0])
+	cost = costs[-1]
 
-    """ Update Function """
-    f_grad, f_update = adadelta(tparams, t_f, t_n, omega, cost, grads)
-    return f_grad, f_update
+
+	""" Gradient """
+	grads = theano.grad(cost, wrt=list(tparams.values()))
+
+
+	""" Update Function """
+	f_grad, f_update = adadelta(tparams, t_f, t_n, omega, cost, grads)
+	return f_grad, f_update
 
 def train(data_opt, train_opt):
 	print 'Initializing data provider...'
@@ -257,7 +267,9 @@ def train(data_opt, train_opt):
 	uidx = 0
 	prev_log_like = -np.inf
 	omega = np.array([i*2*np.pi/train_opt['dim_feq'] for i in range(train_opt['dim_feq'])])
+	print (train_opt['max_epoch'])
 	for eidx in range(train_opt['max_epoch']):
+		print (dp_tr.get_batch_num())
 		for bidx in range(dp_tr.get_batch_num()):
 			[epoch, batch, [data, nframe]] = dp_tr.get_next_batch()
 
@@ -286,13 +298,33 @@ def train(data_opt, train_opt):
 				if test_log_like > prev_log_like:
 					print 'Best parameter so far found. Saving...'
 					param = unzip(tparams)
+					#break
+					weightsz = params.get("W_z")
+					abc = np.zeros((8, 30), dtype=int)
+					for i in range(88):
+						array = weightsz[:, i, :]
+						abc = np.add(abc, array)
+					print(abc)
+					df = pd.DataFrame(abc)
+					name = "W_z.xlsx"
+					df.to_excel(name, index=False)
+					#break
+					weightso = params.get("U_o")
+					cde = np.zeros((8, 30), dtype=int)
+					for i in range(88):
+						array = weightso[:, i, :]
+						cde = np.add(abc, array)
+					print(cde)
+					df = pd.DataFrame(cde)
+					name2 = "U_o.xlsx"
+					df.to_excel(name2, index=False)
 					fo = open(train_opt['save_dir'], 'wb')
 					pickle.dump({'param': param, 'log': test_log_like}, fo, protocol=pickle.HIGHEST_PROTOCOL)
 					fo.close()
 					prev_log_like = test_log_like
 			uidx+=1
 
-def param_init(mode, train_opt, input_dim, isAdp=False):
+def param_init(mode, train_opt, input_dim, isAdp=True):
 	if mode == 'train':
 		params = OrderedDict()
 		params['W_ste'] = pre_proc(np.random.randn(train_opt['dim'], train_opt['dim_pitch']))
@@ -332,9 +364,9 @@ def param_init(mode, train_opt, input_dim, isAdp=False):
 
 if __name__ == '__main__':
 
-	data_opt = {'dir_path': '/path/to/dir', 'pitch_range': (21, 109), 'dt': 0.3, 'batch_size': 20}
-	train_opt = {'max_epoch': 100, 'lrate': 0.0001, 'dim': 50, 'dim_feq': 4, 'dim_pitch': 88, 'test_freq':15, 'isAdp': False,\
-				'continue_dir': '/path/to/continue_dir', 'save_dir': '/path/to/save_dir'}
+	data_opt = {'dir_path': 'data', 'pitch_range': (15, 124), 'dt': 0.3, 'batch_size': 1}
+	train_opt = {'max_epoch': 10000, 'lrate': 0.0001, 'dim': 30, 'dim_feq': 8, 'dim_pitch': 109, 'test_freq':15, 'isAdp': True,\
+				'continue_dir': 'continue', 'save_dir': 'save'}
 
 	train(data_opt, train_opt)
 
